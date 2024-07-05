fc134f00546f61ce278d9ab3b9e5bf3eaf08a28f
Priyanshu Tiwari <priyanshut98283@gmail.com>
2024-07-05 20:25:33 +0530

    Production code

--- build_files.sh
+++
@@ -1,9 +0,0 @@
-# Use a specific Python version if needed
-PYTHON=$(which python3)
-PIP=$(which pip3)
-
-# Install Python dependencies
-$PIP install -r requirements.txt
-
-# Collect static files
-$PYTHON manage.py collectstatic --noinput
